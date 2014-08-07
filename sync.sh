54eadef434325ad1a03a3e8a0c1c7fd988f36aa6
Aaron Suggs <aaron@ktheory.com>
2014-08-07 04:18:47 -0400

    Quiet output

--- sync.sh
+++ sync.sh
@@ -5,11 +5,11 @@ if [ -z "$S3_BUCKET" ] || [ -z "$S3_KEY" ] || [ -z "$DESTINATION" ]; then
   exit 1
 fi
 
-# Set umask
+# Optionally set umask for destination file permissions
 if [ -n "$UMASK" ]; then
   umask "$UMASK"
 fi
 
 # Fetch to temp file to move atomically
-aws s3api get-object --bucket $S3_BUCKET --key $S3_KEY /tmp/out
+aws s3api get-object --bucket $S3_BUCKET --key $S3_KEY /tmp/out > /dev/null
 mv /tmp/out $DESTINATION
