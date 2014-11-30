423103d61141e72ca6819211724e2291cb07186f
Aaron Suggs <aaron@ktheory.com>
2014-11-30 12:23:01 -0500

    Run in a loop with INTERVAL option

--- sync.sh
+++ sync.sh
@@ -7,11 +7,27 @@ fi
 
 ##
 # Fetch file for S3, move it in place atomically
-aws s3api get-object --bucket $S3_BUCKET --key $S3_KEY /tmp/out > /dev/null
+function do_sync {
+  aws s3api get-object --bucket $S3_BUCKET --key $S3_KEY /tmp/out > /dev/null
 
-# Optionally set file permissions
-if [ -n "$MODE" ]; then
-  chmod "$MODE" /tmp/out
+  # Optionally set file permissions
+  if [ -n "$MODE" ]; then
+    chmod "$MODE" /tmp/out
+  fi
+
+  mv /tmp/out $DESTINATION
+}
+
+if [ -z "$INTERVAL" ]; then
+  # Run once
+  do_sync
+else
+  # Loop every $INTERVAL seconds
+  while true; do
+    s=`date +'%s'`
+
+    do_sync
+
+    sleep $(( $INTERVAL - (`date +'%s'` - $s) ))
+  done
 fi
-
-mv /tmp/out $DESTINATION
