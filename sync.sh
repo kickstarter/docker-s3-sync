9009f5c72cccbadca0322e098db12d3f6c0dcfea
Antoine Cotten <tonio.cotten@gmail.com>
2015-04-24 20:02:36 +0200

    support put action

--- sync.sh
+++ sync.sh
@@ -1,13 +1,13 @@
 #!/bin/bash -e
 
-if [ -z "$S3_BUCKET" ] || [ -z "$S3_KEY" ] || [ -z "$DESTINATION" ]; then
-  echo "Must set S3_BUCKET, S3_KEY, and DESTINATION env vars" 1>&2
+if [ -z "$S3_BUCKET" ] || [ -z "$S3_KEY" ] || [ -z "$LOCALFILE" ] || [ -z "$ACTION" ]; then
+  echo "Must set S3_BUCKET, S3_KEY, LOCALFILE and ACTION env vars" 1>&2
   exit 1
 fi
 
 ##
 # Fetch file for S3, move it in place atomically
-function do_sync {
+function fetch {
   aws s3api get-object --bucket $S3_BUCKET --key $S3_KEY /tmp/out > /dev/null
 
   # Optionally set file permissions
@@ -15,18 +15,24 @@ function do_sync {
     chmod "$MODE" /tmp/out
   fi
 
-  mv /tmp/out $DESTINATION
+  mv /tmp/out $LOCALFILE
+}
+
+##
+# Put file to S3
+function put {
+  aws s3api put-object --bucket $S3_BUCKET --key $S3_KEY --body $LOCALFILE > /dev/null
 }
 
 if [ -z "$INTERVAL" ]; then
   # Run once
-  do_sync
+  $ACTION
 else
   # Loop every $INTERVAL seconds
   while true; do
     s=`date +'%s'`
 
-    do_sync
+    $ACTION
 
     sleep $(( $INTERVAL - (`date +'%s'` - $s) ))
   done
